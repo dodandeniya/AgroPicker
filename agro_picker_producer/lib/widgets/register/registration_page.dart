d24639737371dbb8b3ebf4aef8c4ef5465fcfd06
GowthamF <40605124+GowthamF@users.noreply.github.com>
2020-06-06 22:05:11 +0530

    adding steps initial

--- agro_picker_producer/lib/widgets/register/registration_page.dart
+++ agro_picker_producer/lib/widgets/register/registration_page.dart
@@ -1,7 +1,11 @@
-import 'package:agro_picker_producer/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:agro_picker_producer/agro_picker_producer.dart';
 
 class RegistrationPage extends StatefulWidget {
+  final int stepIndex;
+
+  RegistrationPage({this.stepIndex});
+
   @override
   State<StatefulWidget> createState() {
     return _RegistrationPage();
@@ -22,169 +26,83 @@ class _RegistrationPage extends State<RegistrationPage> {
       TextEditingController();
   bool isLoading = true;
   final _formKey = GlobalKey<FormState>();
+  int stepIndex = 0;
+  List<Step> steps = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height,
       color: Theme.of(context).scaffoldBackgroundColor,
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: Form(
-                key: _formKey,
-                child: Column(
+      child: Container(
+        height: 500,
+        margin: EdgeInsets.only(top: 10),
+        child: Stepper(
+            type: StepperType.horizontal,
+            currentStep: widget.stepIndex,
+            steps: getSteps(context),
+            onStepContinue: () {},
+            onStepCancel: () {},
+            controlsBuilder: (context,
+                    {VoidCallback onStepContinue, VoidCallback onStepCancel}) =>
+                Column(
                   children: <Widget>[
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Enter Name';
-                          }
-                          return null;
-                        },
-                        controller: _nameEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'Name',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 15),
-                        ),
-                      ),
+                    SizedBox(
+                      height: 50,
                     ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Select Gender';
-                          }
-                          return null;
-                        },
-                        controller: _genderEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'Gender',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 10),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Enter NIC/Passport';
-                          }
-                          return null;
-                        },
-                        controller: _nicPassportEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'NIC/Passport',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 10),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Enter Province/State';
-                          }
-                          return null;
-                        },
-                        controller: _provinceEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'Province/State',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 10),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Enter Mobile Number';
-                          }
-                          return null;
-                        },
-                        keyboardType: TextInputType.phone,
-                        controller: _mobileNumberEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'Mobile Number',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 10),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please Enter Password';
-                          }
-                          return null;
-                        },
-                        controller: _passwordEditingController,
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)),
-                          contentPadding: EdgeInsets.only(
-                              top: 0, bottom: 10, left: 10, right: 15),
-                        ),
-                      ),
-                    ),
-                    RaisedButton(
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {}
-                          // setState(() {
-                          //   isLoading = false;
-                          // });
-                        },
-                        child: isLoading
-                            ? Text(
-                                'Save',
-                                style: TextStyle(color: Colors.white),
-                              )
-                            : Container(
-                                child: CircularProgressIndicator(
-                                  backgroundColor: Colors.white,
-                                  valueColor: AlwaysStoppedAnimation<Color>(
-                                      Colors.lightBlue),
-                                ),
-                                width: 25,
-                                height: 25,
-                              )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Container(),
+                        Container(),
+                      ],
+                    )
                   ],
-                ),
-              ),
-            ),
-          ],
-        ),
+                )),
       ),
     );
   }
+
+  List<Step> getSteps(BuildContext context) {
+    steps = [
+      Step(
+        title: Container(),
+        content: Text('General Information'),
+        state: _getState(0),
+      ),
+      Step(
+        title: Container(),
+        content: Text('General Information1'),
+        state: _getState(1),
+      ),
+      Step(
+        title: Container(),
+        content: Text('General Information2'),
+        state: _getState(2),
+      ),
+    ];
+
+    return steps;
+  }
+
+  StepState _getState(int index) {
+    if (widget.stepIndex > index) {
+      return StepState.complete;
+    }
+    return StepState.indexed;
+  }
+
+  void stepNext() {
+    setState(() {
+      if (stepIndex != steps.length) {
+        stepIndex++;
+      }
+    });
+  }
+
+  void stepPrevious() {}
 }
--- agro_picker_producer/lib/widgets/registration.dart
+++ agro_picker_producer/lib/widgets/registration.dart
@@ -1,16 +1,68 @@
-import 'package:agro_picker_producer/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:agro_picker_producer/agro_picker_producer.dart';
 
-class Registration extends StatelessWidget {
+class Registration extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Registration();
+  }
+}
+
+class _Registration extends State<Registration> {
+  int stepIndex = 0;
+  int stepsCount = 3;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: Text('Register'),
+        ),
         body: GestureDetector(
-            onTap: () {
-              FocusScope.of(context).unfocus();
-            },
-            child: RegistrationPage()),
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Column(
+            children: <Widget>[
+              SingleChildScrollView(
+                child: RegistrationPage(
+                  stepIndex: stepIndex,
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomSheet: Card(
+          elevation: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              stepIndex == 0
+                  ? Container(
+                      height: 0,
+                    )
+                  : FlatButton(
+                      onPressed: () {
+                        if (stepIndex > 0) {
+                          setState(() {
+                            stepIndex--;
+                          });
+                        }
+                      },
+                      child: Text('Previous')),
+              stepIndex == stepsCount - 1
+                  ? FlatButton(onPressed: () {}, child: Text('Finish'))
+                  : FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          stepIndex++;
+                        });
+                      },
+                      child: Text('Next'))
+            ],
+          ),
+        ),
       ),
     );
   }
